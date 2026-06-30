412958544ca84c8405ee3789132d4d1518d1e26a
PRANJALRANA11 <pranajlrana1235@gmail.com>
2026-06-30 15:09:35 +0530

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,8 +32,6 @@ export default function RootLayout({
       appearance={{
         baseTheme: [dark],
       }}
-      signInUrl="/signin"
-      signUpUrl="/signup"
     >
       <AppProvider>
         <MyRuntimeProvider>
